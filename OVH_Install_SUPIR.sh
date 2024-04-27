3e0328f8e189c4ebd5824cffc3326d2af94c45cb
Azdin Mediri <a.elmediri@gmail.com>
2024-04-27 10:06:17 +0200

    download

--- OVH_Install_SUPIR.sh
+++ OVH_Install_SUPIR.sh
@@ -1,4 +1,4 @@
-pip3 install requests
+pip3 install requests --break-system-packages
 
 git clone https://github.com/FurkanGozukara/SUPIR
 
@@ -8,14 +8,14 @@ python3 -m venv venv
 
 source ./venv/bin/activate
 
-pip3 install -r requirements.txt
+pip3 install -r requirements.txt --break-system-packages
 
 echo "Installing requirements"
 
-pip3 install torch==2.2.0 torchvision torchaudio --index-url https://download.pytorch.org/whl/cu121 --upgrade
-pip3 install triton
-pip3 install xformers==0.0.24
-pip3 install bitsandbytes==0.43.0 --upgrade
+pip3 install torch==2.2.0 torchvision torchaudio --index-url https://download.pytorch.org/whl/cu121 --upgrade --break-system-packages
+pip3 install triton --break-system-packages
+pip3 install xformers==0.0.24 --break-system-packages
+pip3 install bitsandbytes==0.43.0 --upgrade --break-system-packages
 
 python3 download_models.py
 
